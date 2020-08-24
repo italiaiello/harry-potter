5554d3b75e1e3d51697374fb5a0bd9c033eb2a54
Andrew Aiello <andrewaiello7@gmail.com>
2020-08-24 19:30:45 +1000

    added search bar styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import MenuItem from '../MenuItem/MenuItem';
 import { API_KEY } from '../../pages/MainPage/MainPage';
+import HogwartsLogo from '../../assets/images/hogwartsLogo.jpg';
+import Characters from '../../assets/images/characters.jpg';
+import Spells from '../../assets/images/spells.jpg';
+import SortingHat from '../../assets/images/sortingHatOption.jpg';
 
 const Menu = ({ dispatch }) => {
 
     return (
         <section id="menuContainer">
-            <h2 id="menuHeading">Welcome to the Harry Potter API</h2>
-            <h3>Find all the info you need about the series</h3>
+            <h1 id="menuHeading">Welcome to the Harry Potter API</h1>
+            <h3>Select one of the options below</h3>
             <section id="menu">
-                <h4>What would you like to do?</h4>
                 <section id="menuItemContainer">
-                    <MenuItem dispatch={dispatch} route={'characters'} url={`characters?key=${API_KEY}`} title={'Characters'} />
-                    <MenuItem dispatch={dispatch} route={'houses'} url={`houses?key=${API_KEY}`} title={'Houses'} />
-                    <MenuItem dispatch={dispatch} route={'spells'} url={`spells?key=${API_KEY}`} title={'Spells'} />
-                    <MenuItem dispatch={dispatch} route={'sortingHat'} url={`sortingHat?key=${API_KEY}`} title={'Sorting Hat'} />
+                    <MenuItem dispatch={dispatch} route={'characters'} url={`characters?key=${API_KEY}`} title={'Characters'} image={Characters}/>
+                    <MenuItem dispatch={dispatch} route={'houses'} url={`houses?key=${API_KEY}`} title={'Houses'} image={HogwartsLogo}/>
+                    <MenuItem dispatch={dispatch} route={'spells'} url={`spells?key=${API_KEY}`} title={'Spells'} image={Spells} />
+                    <MenuItem dispatch={dispatch} route={'sortingHat'} url={`sortingHat?key=${API_KEY}`} title={'Sorting Hat'} image={SortingHat} />
                 </section>
             </section>
         </section>
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { ACTIONS } from '../../pages/MainPage/MainPage';
 
-const MenuItem = ({dispatch, route, url, title}) => {
+const MenuItem = ({dispatch, route, url, title, image}) => {
 
     return (
-        <article    
-            className="menuItem option" 
+        <article
+            style={{backgroundImage: `url(${image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}    
+            className="menuItem" 
             onClick={() => {
                 dispatch({ type: ACTIONS.CHANGE_ROUTE, payload: { route: route } })
                 dispatch({ type: ACTIONS.SET_URL, payload: { url: url } })
